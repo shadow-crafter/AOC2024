2f50f2e5db3da13d491b416145620a415ccb2f69
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2024-12-01 22:08:35 -0500

    Start puzzle day 1

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,13 @@
-print("Hello, advent of code!")
+location_list = []
+
+def process_input():
+    with open("..\input.txt") as f:
+        for line in f:
+            location_list.append(line.strip().split("   "))
+
+def main():
+    process_input()
+    print(location_list)
+
+if __name__ == "__main__":
+    main()
