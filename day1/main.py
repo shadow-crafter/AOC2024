ecb243b30e40f865ad58d6f44d2d2d97f046d737
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2024-12-03 19:16:56 -0500

    Finish day 1

--- day1/main.py
+++ day1/main.py
@@ -1,13 +1,37 @@
-location_list = []
+location_list = [[], []]
 
 def process_input():
     with open("..\input.txt") as f:
         for line in f:
-            location_list.append(line.strip().split("   "))
+            split = line.strip().split("   ")
+            location_list[0].append(int(split[0]))
+            location_list[1].append(int(split[1]))
+        location_list[0].sort()
+        location_list[1].sort()
+
+def find_distances(locations):
+    distances = []
+    for i in range(len(locations[0])):
+        distances.append(abs(locations[0][i] - locations[1][i]))
+    return distances
+
+def find_frequency(locations):
+    frequency_vals = []
+    for i in range(len(locations[0])):
+        frequency_vals.append(locations[1].count(locations[0][i]) * locations[0][i])
+    return frequency_vals
 
 def main():
     process_input()
     print(location_list)
+
+    distances = find_distances(location_list)
+    total_distance = sum(distances)
+    print("Total of the distances: " + str(total_distance))
+
+    frequencies = find_frequency(location_list)
+    total_similarity = sum(frequencies)
+    print("Total of similarity scores: " + str(total_similarity))
 
 if __name__ == "__main__":
     main()
